9d13b2a3dcabfced84fe217fd71df0fe6d720f7b
krishnakasireddy <104017310+krishnakasireddy@users.noreply.github.com>
2023-07-14 20:37:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Integer/minimum-integer.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def minimumInteger(self, N : int, A : List[int]) -> int:
+        a = sum(A)
+        B = list(set(A))
+        B.sort()
+        for i in B:
+            if N*i >= a:
+                return i
+                break
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        A=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.minimumInteger(N, A)
+        
+        print(res)
+        
+
+# } Driver Code Ends
