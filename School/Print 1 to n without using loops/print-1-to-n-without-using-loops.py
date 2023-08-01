2b7f10f0632d171b11a022becb0300a81a00bd4b
krishnakasireddy <104017310+krishnakasireddy@users.noreply.github.com>
2023-08-01 23:26:03 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 to n without using loops/print-1-to-n-without-using-loops.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def printTillN(self, N):
+    	for i in range (1,N+1):
+    	    print(i,end=" ")
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTillN(N)
+        print()
+# } Driver Code Ends
