4d36302791d58a41ba8107a52e39395d031f6f1b
krishnakasireddy <104017310+krishnakasireddy@users.noreply.github.com>
2023-08-14 23:42:55 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find Index/find-index.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def findIndex (self,a, N, key ):
+        start,end = -1,-1
+        for i in range (0,N):
+            if a[i] == key:
+                start = i
+                break
+        for i in range (N-1,-1,-1):
+            if a[i] == key:
+                end = i
+                break
+        return start,end
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    key=int(input())
+    ob = Solution()
+    ans=ob.findIndex(a, n, key )
+    print(*ans)
+    
+# } Driver Code Ends
