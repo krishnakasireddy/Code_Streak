a95082d500835895cf7284da45c8ede30205e4a2
krishnakasireddy <104017310+krishnakasireddy@users.noreply.github.com>
2023-09-30 23:08:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Panagram Checking/panagram-checking.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if a string is Pangram or not.
+    def checkPangram(self,s):
+        s = s.lower()
+        count = 0
+        l = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for i in l:
+            if i in s:
+                count+=1
+        if count==26:
+            return 1
+        else:
+            return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        if(obj.checkPangram(s)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
