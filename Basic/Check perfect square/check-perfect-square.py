985b82aacc513ad065f73338844f302c439da288
krishnakasireddy <104017310+krishnakasireddy@users.noreply.github.com>
2023-10-16 23:58:43 +0530

    Updated Solution - GfG to GitHub

--- Basic/Check perfect square/check-perfect-square.py
+++ Basic/Check perfect square/check-perfect-square.py
@@ -1,14 +1,19 @@
 #User function Template for python3
+
+#User function Template for python3
 import math
 class Solution:
-    def checkPerfectSquare (ob,N):
-        k = math.sqrt(N)
-        a = math.floor(k)
-        b = math.ceil(k)
+    def isPerfectSquare (self, n):
+        k = math.sqrt(n)
+        a = math.ceil(k)
+        b = math.floor(k)
         if a == b:
             return 1
         else:
             return 0
+
+
+
 
 
 #{ 
@@ -17,9 +22,8 @@ class Solution:
 if __name__ == '__main__': 
     t = int (input ())
     for _ in range (t):
+        n = int(input())
         
-        N=int(input())
-
         ob = Solution()
-        print(ob.checkPerfectSquare(N))
+        print(ob.isPerfectSquare(n))
 # } Driver Code Ends
